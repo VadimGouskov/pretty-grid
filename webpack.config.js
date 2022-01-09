6bb3af1b478f3a4220d531c73de4250b929aab9e
VadimGouskov <vadimgouskov@hotmail.com>
2022-01-09 17:03:21 +0100

    bundled to one file using webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/pretty-grid.ts',
+  devtool: 'inline-source-map',
+  mode:"production",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'pretty-grid.js',
+    library: {
+        name: 'prettyGrid',
+        type: 'umd',
+      },
+  },
+};
