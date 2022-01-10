fe7b8b38c58c0eb25205255f31a7c6c081a115fc
VadimGouskov <vadimgouskov@hotmail.com>
2022-01-10 21:05:43 +0100

    added first jsdocs

--- dist/grid.d.ts
+++ dist/grid.d.ts
@@ -5,12 +5,48 @@ export declare class GridPoint {
 }
 export declare class Grid {
     private points;
+    /**
+     * @constructor
+     * Instantiates a new 2 Dimensional Grid.
+     * The horizontal distance between each column: width / (cols - 1)
+     * The vertical distance between each row : height / (rows - 1)
+     * @param cols
+     * @param rows
+     * @param width
+     * @param height
+     */
     constructor(cols: number, rows: number, width: number, height: number);
     get flat(): GridPoint[];
+    /**
+     * Get all the current points on the grid
+     * warning: gets the points array by reference. Changes to individual points will be reflected in the original grid object.
+     * To get a deep copy use grid.copy(). eg. grid.copy.get()
+     */
     get(): GridPoint[][];
+    /**
+     * Set / replace all the current points on the grid
+     * warning: sets a reference to the provided points. Changes in made by this grid object to the points will be reflected in the provided points array.
+     */
     set(points: GridPoint[][]): void;
+    /**
+     * Gets a point from from indeces [col, row]
+     * @param col
+     * @param row
+     */
     getPoint(col: number, row: number): GridPoint;
+    /**
+     * Loops over the points in the grid, passing each point to the provided @param func
+     * Provide a drawing function
+     * @param func a function that handles drawing of each individual point
+     */
     draw(func: (point: GridPoint) => void): void;
+    /**
+     * Translates the entire grid by @param x en @param y coordinates
+     * @param x
+     * @param y
+     */
     translate(x: number, y: number): void;
+    /** Creates a deep copy of the current grid object
+     */
     copy(): Grid;
 }
--- src/grid.ts
+++ src/grid.ts
@@ -10,6 +10,16 @@ export class GridPoint {
 export class Grid {
     private points: GridPoint[][] = [[]];
 
+    /**
+     * @constructor
+     * Instantiates a new 2 Dimensional Grid.
+     * The horizontal distance between each column: width / (cols - 1)
+     * The vertical distance between each row : height / (rows - 1)
+     * @param cols
+     * @param rows
+     * @param width
+     * @param height
+     */
     constructor(cols: number, rows: number, width: number, height: number) {
         const stepCols = width / (cols - 1);
         const stepRows = height / (rows - 1);
@@ -27,22 +37,48 @@ export class Grid {
         return this.points.reduce((acc, val) => acc.concat(val), []);
     }
 
+    /**
+     * Get all the current points on the grid
+     * warning: gets the points array by reference. Changes to individual points will be reflected in the original grid object.
+     * To get a deep copy use grid.copy(). eg. grid.copy.get()
+     */
     get(): GridPoint[][] {
         return this.points;
     }
 
+    /**
+     * Set / replace all the current points on the grid
+     * warning: sets a reference to the provided points. Changes in made by this grid object to the points will be reflected in the provided points array.
+     */
+    // TODO set a deep copy makes more sence? or make it optional and provide a deep copy helper function
     set(points: GridPoint[][]): void {
         this.points = points;
     }
+
+    /**
+     * Gets a point from from indeces [col, row]
+     * @param col
+     * @param row
+     */
 
     getPoint(col: number, row: number): GridPoint {
         return this.points[col][row];
     }
 
+    /**
+     * Loops over the points in the grid, passing each point to the provided @param func
+     * Provide a drawing function
+     * @param func a function that handles drawing of each individual point
+     */
     draw(func: (point: GridPoint) => void): void {
         this.flat.forEach((point) => func(point));
     }
 
+    /**
+     * Translates the entire grid by @param x en @param y coordinates
+     * @param x
+     * @param y
+     */
     translate(x: number, y: number): void {
         this.points.forEach((col) =>
             col.forEach((point) => {
@@ -52,6 +88,8 @@ export class Grid {
         );
     }
 
+    /** Creates a deep copy of the current grid object
+     */
     copy(): Grid {
         const cols = this.points.length;
         // TODO take into account posibility of modified row
