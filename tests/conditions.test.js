2f7a774d3f67fff5303acd9650cd5c5f2ef96af7
VadimGouskov <vadimgouskov@hotmail.com>
2022-01-25 15:06:22 -0500

    added first conditions tests

---
+++ tests/conditions.test.js
@@ -0,0 +1,21 @@
+const { Grid, even, odd } = require('../dist/pretty-grid.js');
+
+const GRID_ROWS = 5;
+const GRID_COLS = 5;
+const WIDTH = 100;
+const HEIGHT = 100;
+const grid = new Grid(GRID_COLS, GRID_ROWS, WIDTH, HEIGHT);
+
+test('Even Condition', () => {
+    grid.get().forEach((col, colİndex) => col.forEach((point, rowİndex) => {
+        const result = even()(point, colİndex, rowİndex);
+        expect(result).toBe((colİndex % 2 === 0 && rowİndex % 2 === 0));
+    } ))
+})
+
+test('Odd Condition', () => {
+    grid.get().forEach((col, colİndex) => col.forEach((point, rowİndex) => {
+        const result = odd()(point, colİndex, rowİndex);
+        expect(result).toBe((colİndex % 2 !== 0 && rowİndex % 2 !== 0));
+    } ))
+})
