b1553c704a6f94b9893b6a49bb852265937cf692
Vadim Gouskov <vadimgouskov@hotmail.com>
2023-03-22 07:43:57 +0100

    fix: static site generation

--- website/src/components/examples/example.tsx
+++ website/src/components/examples/example.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Sketch from "react-p5";
 import p5Types from "p5"; //Import this for typechecking and intellisense
 import { Box, Code, Heading, Stack, Text } from "@chakra-ui/react";
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { stackoverflowDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import BrowserOnly from "@docusaurus/BrowserOnly";
+import useIsBrowser from "@docusaurus/useIsBrowser";
 
 interface Props {
     setup: (p5: p5Types, canvasParentRef: Element) => void
@@ -17,12 +17,17 @@ interface Props {
 export const Example: React.FC<Props> = (props) => {
 
     const sectionId = props.title.replace(/ /g, "-")
+    const isBrowser = useIsBrowser();
+
     return (
         <Stack direction={{ base: "column-reverse", md: "row" }} id={sectionId} >
-            <Box>
-                {/* <BrowserOnly>
-                    {() => <Sketch setup={props.setup} draw={props.draw} />}
-                </BrowserOnly> */}
+            <Box paddingTop={"2.5rem"}>
+                <BrowserOnly fallback={<></>}>
+                    {() => {
+                        const Sketch = require('react-p5');
+                        return <Sketch setup={props.setup} draw={props.draw} />
+                    }}
+                </BrowserOnly>
             </Box>
             <Box width="100%" maxWidth="100%">
                 <Heading>
